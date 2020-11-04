41cf71ba5fdce2d1cf761bf7d50916f4f6b75f01
marcelo de lucas <marcelolucas787@gmail.com>
2020-11-04 16:53:58 -0300

    quatro do doze

---
+++ sus.io/Controllers/testCrudController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sus.io.Controllers
+{
+    public class testCrudController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
